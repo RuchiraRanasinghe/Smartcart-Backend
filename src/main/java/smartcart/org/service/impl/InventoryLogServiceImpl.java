33845aa61ebb14301fd30a3b7b04db035649ee44
NipunBasnayake <nipunsathsara1999@gmail.com>
2025-05-18 00:56:56 +0530

    Update InventoryLog Service
    Co-Authored-By: Ruchira Ranasinghe <rhvruchira@gmail.com>

--- src/main/java/smartcart/org/service/impl/InventoryLogServiceImpl.java
+++ src/main/java/smartcart/org/service/impl/InventoryLogServiceImpl.java
@@ -4,13 +4,15 @@ import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 import smartcart.org.dto.InventoryLogDto;
+import smartcart.org.dto.ProductDto;
 import smartcart.org.entity.InventoryLog;
+import smartcart.org.entity.Product;
 import smartcart.org.exception.ResourceNotFoundException;
 import smartcart.org.repository.InventoryLogRepository;
 import smartcart.org.service.InventoryLogService;
+import smartcart.org.service.ProductService;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -18,17 +20,22 @@ public class InventoryLogServiceImpl implements InventoryLogService {
 
     private final InventoryLogRepository inventoryLogRepository;
     private final ModelMapper modelMapper;
+    private final ProductService productService;
+
+    String logNotFoundWithIdMessage = "Inventory log not found with id: ";
 
     @Override
     public InventoryLogDto createLog(InventoryLogDto logDto) {
-        InventoryLog log = modelMapper.map(logDto, InventoryLog.class);
-        return modelMapper.map(inventoryLogRepository.save(log), InventoryLogDto.class);
+        ProductDto productById = productService.getProduct(logDto.getProductId());
+        InventoryLog mappedInventoryLog = modelMapper.map(logDto, InventoryLog.class);
+        mappedInventoryLog.setProduct(modelMapper.map(productById, Product.class));
+        return modelMapper.map(inventoryLogRepository.save(mappedInventoryLog), InventoryLogDto.class);
     }
 
     @Override
     public InventoryLogDto getLogById(Long id) {
         InventoryLog log = inventoryLogRepository.findById(id)
-                .orElseThrow(() -> new ResourceNotFoundException("Log not found with id: " + id));
+                .orElseThrow(() -> new ResourceNotFoundException(logNotFoundWithIdMessage + id));
         return modelMapper.map(log, InventoryLogDto.class);
     }
 
@@ -37,22 +44,21 @@ public class InventoryLogServiceImpl implements InventoryLogService {
         return inventoryLogRepository.findAll()
                 .stream()
                 .map(log -> modelMapper.map(log, InventoryLogDto.class))
-                .collect(Collectors.toList());
+                .toList();
     }
 
     @Override
     public InventoryLogDto updateLog(Long id, InventoryLogDto logDto) {
-        InventoryLog existing = inventoryLogRepository.findById(id)
-                .orElseThrow(() -> new ResourceNotFoundException("Log not found with id: " + id));
-
-        modelMapper.map(logDto, existing);
-        return modelMapper.map(inventoryLogRepository.save(existing), InventoryLogDto.class);
+        ProductDto productById = productService.getProduct(logDto.getProductId());
+        InventoryLog mappedInventoryLog = modelMapper.map(logDto, InventoryLog.class);
+        mappedInventoryLog.setProduct(modelMapper.map(productById, Product.class));
+        return modelMapper.map(inventoryLogRepository.save(mappedInventoryLog), InventoryLogDto.class);
     }
 
     @Override
     public Boolean deleteLog(Long id) {
         InventoryLog existing = inventoryLogRepository.findById(id)
-                .orElseThrow(() -> new ResourceNotFoundException("Log not found with id: " + id));
+                .orElseThrow(() -> new ResourceNotFoundException(logNotFoundWithIdMessage + id));
         inventoryLogRepository.delete(existing);
         return true;
     }
