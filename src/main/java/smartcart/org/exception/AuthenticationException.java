6c1837896478d68d754242ef51053d47ed798c52
SahanChamara <sahanchamara456@gmail.com>
2025-05-28 10:58:31 +0530

    handle unauthorized error in login

---
+++ src/main/java/smartcart/org/exception/AuthenticationException.java
@@ -0,0 +1,7 @@
+package smartcart.org.exception;
+
+public class AuthenticationException extends RuntimeException {
+    public AuthenticationException(String message) {
+        super(message);
+    }
+}
--- src/main/java/smartcart/org/exception/GlobalExceptionHandler.java
+++ src/main/java/smartcart/org/exception/GlobalExceptionHandler.java
@@ -4,13 +4,14 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import smartcart.org.response.ApiResponse;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(ResourceNotFoundException.class)
-    public ResponseEntity<String> handleResourceNotFoundException(ResourceNotFoundException ex) {
-        return new ResponseEntity<>(ex.getMessage(), HttpStatus.NOT_FOUND);
+    public ResponseEntity<ApiResponse<Object>> handleResourceNotFoundException(ResourceNotFoundException ex) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new ApiResponse<>(false,HttpStatus.NOT_FOUND.value(),null,ex.getMessage() ));
     }
 
     @ExceptionHandler(UserAlreadyExistsException.class)
@@ -26,5 +27,10 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(Exception.class)
     public ResponseEntity<String> handleGeneralException(Exception ex) {
         return new ResponseEntity<>("An error occurred: " + ex.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @ExceptionHandler(AuthenticationException.class)
+    public ResponseEntity<ApiResponse<Object>> handleAuthenticationException(AuthenticationException ex){
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(new ApiResponse<>(false,HttpStatus.UNAUTHORIZED.value(), null,ex.getMessage()));
     }
 }
