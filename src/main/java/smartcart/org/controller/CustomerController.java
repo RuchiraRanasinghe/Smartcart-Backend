cb403b54e4e95eca2eac9767df98e81e4abb5f65
Sahan Chamara <sahanchamara456@gmail.com>
2025-06-08 22:40:27 +0530

    customer controller api response edit

--- src/main/java/smartcart/org/controller/CustomerController.java
+++ src/main/java/smartcart/org/controller/CustomerController.java
@@ -1,10 +1,13 @@
 package smartcart.org.controller;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import smartcart.org.dto.CustomerDto;
+import smartcart.org.response.ApiResponse;
 import smartcart.org.service.CustomerService;
 
 import java.util.List;
@@ -18,34 +21,34 @@ public class CustomerController {
     private final CustomerService customerService;
 
     @PostMapping
-    public ResponseEntity<CustomerDto> createCustomer(@RequestBody CustomerDto customer) {
+    public ResponseEntity<ApiResponse<CustomerDto>> createCustomer(@Valid @RequestBody CustomerDto customer) {
         CustomerDto created = customerService.createCustomer(customer);
-        return ResponseEntity.status(201).body(created);
+        return ResponseEntity.status(HttpStatus.CREATED).body(new ApiResponse<>(true, HttpStatus.CREATED.value(), created));
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<CustomerDto> getCustomerById(@PathVariable("id") Long id) {
+    public ResponseEntity<ApiResponse<CustomerDto>> getCustomerById(@PathVariable("id") Long id) {
         CustomerDto customer = customerService.getCustomerById(id);
-        return ResponseEntity.ok(customer);
+        return ResponseEntity.status(HttpStatus.OK).body(new ApiResponse<>(true,HttpStatus.OK.value(), customer));
     }
 
     @GetMapping
-    public ResponseEntity<List<CustomerDto>> getAllCustomers() {
+    public ResponseEntity<ApiResponse<List<CustomerDto>>> getAllCustomers() {
         List<CustomerDto> customers = customerService.getAllCustomers();
-        return ResponseEntity.ok(customers);
+        return ResponseEntity.status(HttpStatus.OK).body(new ApiResponse<>(true,HttpStatus.OK.value(), customers));
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity<CustomerDto> updateCustomer(@PathVariable("id") Long id, @RequestBody CustomerDto customer) {
+    public ResponseEntity<ApiResponse<CustomerDto>> updateCustomer(@PathVariable("id") Long id,@Valid @RequestBody CustomerDto customer) {
         CustomerDto updated = customerService.updateCustomer(id, customer);
-        return ResponseEntity.ok(updated);
+        return ResponseEntity.status(HttpStatus.OK).body(new ApiResponse<>(true,HttpStatus.OK.value(), updated));
     }
 
     @DeleteMapping("/{id}")
-    public ResponseEntity<String> deleteCustomer(@PathVariable("id") Long id) {
+    public ResponseEntity<ApiResponse<Object>> deleteCustomer(@PathVariable("id") Long id) {
         if (Boolean.TRUE.equals(customerService.deleteCustomer(id))) {
-            return ResponseEntity.status(201).body("Customer deleted successfully");
+            return ResponseEntity.status(HttpStatus.CREATED).body(new ApiResponse<>(true,HttpStatus.CREATED.value(), null,"Customer deleted successfully"));
         }
-        return ResponseEntity.status(404).body("Customer not found");
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new ApiResponse<>(true,HttpStatus.NOT_FOUND.value(), null,"Customer Not Found"));
     }
 }
